dcc37eca9ccc5570f2c4c9bdc800fef2e456b6a4
Shri <thisshri@gmail.com>
2020-08-04 16:01:46 +0530

    Add component shell for /

--- src/containers/Appointment/index.js
+++ src/containers/Appointment/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
+import {
+  useParams,
+  Link,
+} from 'react-router-dom';
+
 import './styles.scss';
 
-const Appointment = () => (
-  <h1>Foo</h1>
-)
+import { Container, Button } from 'react-bootstrap';
+
+const Appointment = () => { 
+  let { date, month, year } = useParams();
+  console.log(date, month, year)
+
+  const timeSlot = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+
+  return (
+    <Container className="AppointmentWrapper">
+      { timeSlot.map(
+          time => (
+            <Link to={`${time}/details/`}>
+              <Button>
+                { time } to { ++time }
+              </Button>
+            </Link>
+          )
+        )
+      }
+    </Container>
+  );
+ }
 
 export default Appointment;
